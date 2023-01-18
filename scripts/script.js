f6c371a0ca0458114a6576e8b38c005031838490
Martina Smiešková <smieskova.martina@gmail.com>
2023-01-18 19:21:55 +0100

    Circles color toggle

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,19 @@
-let myTitle = document.querySelector("h1");
-let myButton = document.querySelector("button");
+let myCircleClass = document.getElementsByClassName('circle');
+
+for (let i = 0; i < myCircleClass.length; i++){
+    myCircleClass[i].addEventListener("click", toggleCircleColor)
+}
+
+function toggleCircleColor () {
+    console.log("circle clicked");
+    this.classList.toggle("blue");
+    this.classList.toggle("red");
+}
+
+// --------------------------------------
+
+let myTitle = document.getElementById("hello");
+let myButton = document.getElementById("piratize");
 
 myButton.addEventListener("click", function(){
     if (myTitle.textContent === "Yarr!") {
